04376e9a5174dbd8489da26abfe5dd7631d830bc
Natanael Copa <ncopa@alpinelinux.org>
2023-01-18 13:21:09 +0100

    make cpuid volatile

--- libbb/hash_md5_sha.c
+++ libbb/hash_md5_sha.c
@@ -17,7 +17,7 @@
 # if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
 static void cpuid(unsigned *eax, unsigned *ebx, unsigned *ecx, unsigned *edx)
 {
-	asm ("cpuid"
+	asm volatile ("cpuid"
 		: "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx)
 		: "0"(*eax),  "2"(*ecx)
 		: "memory"
